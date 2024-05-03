034a4cf4a35b74af1ae31271eea762c3ff6dbe51
Robbie <siboc@student.unimelb.edu.au>
2024-05-03 23:49:49 +1000

    address

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL:"http://52.62.36.163/8800/api",
+    baseURL:"http://52.62.36.163:8800/api",
     withCredentials: true,
 });
 
